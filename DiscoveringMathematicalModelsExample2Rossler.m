a9d18a2e3d815195bb786964772a710766b096c0
agushasan <152897948+agushasan@users.noreply.github.com>
2024-02-05 16:20:29 +0100

    Update DiscoveringMathematicalModelsExample2Rossler.m

--- DiscoveringMathematicalModelsExample2Rossler.m
+++ DiscoveringMathematicalModelsExample2Rossler.m
@@ -1,40 +1,33 @@
 %% Research code by Agus Hasan
+%% Paper: "WyNDA: A Method to Discover Mathematical Models of Dynamical Systems from Data" submitted for MethodsX.
 
 clear;
 clc;
 
 %% time horizon
-tf  = 30;
-dt  = 0.001;
-t   = dt:dt:tf;
+tf  = 20;           % time horizon
+dt  = 0.001;        % time step
+t   = dt:dt:tf;     % time array
 
 %% number of variables and coefficients
-n = 3;
-r = 48;
-
-%% system description
-A = eye(n);
-C = eye(n);
+n = 3;              % number of measured state
+r = 16;             % number of estimated parameters 
 
 %% noise
-R = 0;
+R = 0;              % noise covariance
 
 %% state initialization
-x        = [-8;5;10];
-xbar     = x;%zeros(n,1);
-y        = x;zeros(n,1);
-thetabar = zeros(r,1);
+x        = [-8;5;10];       % actual state initialization
+xbar     = x;               % estimated state initialization
+y        = x;               % measurement initialization
+thetabar = zeros(r,1);      % estimated parameter initialization
  
 %% true parameters
 a = 0.1;
 b = 0.1;
 c = 14;
 
-%% initial control inputs
-%u     = [10 1]';
-
 %% for plotting
-%uArray          = [];
 xArray          = [];
 xbarArray       = [];
 yArray          = [];
@@ -53,32 +46,31 @@ Gamma   = 1*zeros(n,r);
 %% simulation
 for i=1:(tf/dt)
 
-    %uArray         = [uArray u];
     xArray         = [xArray x];
     xbarArray      = [xbarArray xbar];    
     yArray         = [yArray y];
     thetabarArray  = [thetabarArray thetabar]; 
     
-    x = A*x+dt*[-x(2)-x(3);x(1)+a*x(2);b+x(3)*(x(1)-c)];
-    y = C*x+dt*R^2*randn(n,1);
+    x = x+dt*[-x(2)-x(3);x(1)+a*x(2);b+x(3)*(x(1)-c)];
+    y = x+dt*R^2*randn(n,1);
 
-    Phi = [1 y(1) y(2) y(3) y(1)^2 y(2)^2 y(3)^2 y(1)*y(2) y(1)*y(3) y(2)*y(3) sin(y(1)) sin(y(2)) sin(y(3)) cos(y(1)) cos(y(2)) cos(y(3)) zeros(32,1)';
-           zeros(16,1)' 1 y(1) y(2) y(3) y(1)^2 y(2)^2 y(3)^2 y(1)*y(2) y(1)*y(3) y(2)*y(3) sin(y(1)) sin(y(2)) sin(y(3)) cos(y(1)) cos(y(2)) cos(y(3)) zeros(16,1)';
-           zeros(32,1)' 1 y(1) y(2) y(3) y(1)^2 y(2)^2 y(3)^2 y(1)*y(2) y(1)*y(3) y(2)*y(3) sin(y(1)) sin(y(2)) sin(y(3)) cos(y(1)) cos(y(2)) cos(y(3))];
+    Phi = [y(1) y(2) y(3) zeros(13,1)';
+           zeros(3,1)' y(1) y(2) y(3) zeros(10,1)';
+           zeros(6,1)' 1 y(1) y(2) y(3) y(1)^2 y(2)^2 y(3)^2 y(1)*y(2) y(1)*y(3) y(2)*y(3)];
     
     % Estimation using adaptive observer
-    Kx = Px*C'*inv(C*Px*C'+Rx);
-    Kt = Pt*Gamma'*C'*inv(C*Gamma*Pt*Gamma'*C'+Rt);
-    Gamma = (eye(n)-Kx*C)*Gamma;
+    Kx = Px*inv(Px+Rx);
+    Kt = Pt*Gamma'*inv(Gamma*Pt*Gamma'+Rt);
+    Gamma = (eye(n)-Kx)*Gamma;
 
-    xbar = xbar+(Kx+Gamma*Kt)*(y-C*xbar);
-    thetabar = thetabar-Kt*(y-C*xbar);
+    xbar = xbar+(Kx+Gamma*Kt)*(y-xbar);
+    thetabar = thetabar-Kt*(y-xbar);
 
-    xbar = A*xbar+Phi*thetabar;
+    xbar = xbar+Phi*thetabar;
 
     thetabar = thetabar;
-    Px = (1/lambdav)*eye(n)*(eye(n)-Kx*C)*Px*eye(n);
-    Pt = (1/lambdat)*(eye(r)-Kt*C*Gamma)*Pt;
+    Px = (1/lambdav)*eye(n)*(eye(n)-Kx)*Px*eye(n);
+    Pt = (1/lambdat)*(eye(r)-Kt*Gamma)*Pt;
     Gamma = eye(n)*Gamma-Phi;
 
 end
@@ -99,7 +91,7 @@ figure(2)
 subplot(3,2,1)
 plot(t,a*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
-plot(t,thetabarArray(19,:)/dt,':g','LineWidth',10);
+plot(t,thetabarArray(5,:)/dt,':g','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 legend('true','estimated')
 grid on;
@@ -107,7 +99,7 @@ grid minor;
 ylim([0 0.2])
 ylabel('a','FontSize',72)
 subplot(3,2,2)
-plot(t,-a*ones(1,length(t))-thetabarArray(19,:)/dt,':r','LineWidth',10);
+plot(t,-a*ones(1,length(t))-thetabarArray(5,:)/dt,':r','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
@@ -115,14 +107,14 @@ ylabel('error a','FontSize',72)
 subplot(3,2,3)
 plot(t,b*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
-plot(t,thetabarArray(33,:)/dt,':g','LineWidth',10);
+plot(t,thetabarArray(7,:)/dt,':g','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
 ylim([0 0.2])
 ylabel('b','FontSize',72)
 subplot(3,2,4)
-plot(t,b*ones(1,length(t))-thetabarArray(33,:)/dt,':r','LineWidth',10);
+plot(t,b*ones(1,length(t))-thetabarArray(7,:)/dt,':r','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
@@ -130,7 +122,7 @@ ylabel('error b','FontSize',72)
 subplot(3,2,5)
 plot(t,c*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
-plot(t,-thetabarArray(36,:)/dt,':g','LineWidth',10);
+plot(t,-thetabarArray(10,:)/dt,':g','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
@@ -138,11 +130,9 @@ ylim([10 20])
 ylabel('c','FontSize',72)
 xlabel('t (s)')
 subplot(3,2,6)
-plot(t,-c*ones(1,length(t))-thetabarArray(36,:)/dt,':r','LineWidth',10);
+plot(t,-c*ones(1,length(t))-thetabarArray(10,:)/dt,':r','LineWidth',10);
 set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
 ylabel('error c','FontSize',72)
 xlabel('t (s)')
-
-Coeff = [(1/dt)*thetabar(1:(r/n),end)'; (1/dt)*thetabar((r/n)+1:2*(r/n),end)'; (1/dt)*thetabar(2*(r/n)+1:r,end)']
