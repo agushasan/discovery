ab03f8d5205b6a8e40fdfc98a369155588c9d06a
agushasan <152897948+agushasan@users.noreply.github.com>
2024-02-06 09:48:17 +0100

    Update DiscoveringMathematicalModelsExample2Rossler.m

--- DiscoveringMathematicalModelsExample2Rossler.m
+++ DiscoveringMathematicalModelsExample2Rossler.m
@@ -80,7 +80,7 @@ plot3(yArray(1,:),yArray(2,:),yArray(3,:),'-b','LineWidth',16);
 hold on;
 plot3(xbarArray(1,:),xbarArray(2,:),xbarArray(3,:),':r','LineWidth',16)
 legend('measured','estimated')
-set(gca,'color','white','LineWidth',3,'FontSize',56)
+set(gca,'color','white','LineWidth',3,'FontSize',36)
 grid on;
 grid minor;
 xlabel('p')
@@ -92,47 +92,47 @@ subplot(3,2,1)
 plot(t,a*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
 plot(t,thetabarArray(5,:)/dt,':g','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 legend('true','estimated')
 grid on;
 grid minor;
 ylim([0 0.2])
-ylabel('a','FontSize',72)
+ylabel('a','FontSize',36)
 subplot(3,2,2)
 plot(t,-a*ones(1,length(t))-thetabarArray(5,:)/dt,':r','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 grid on;
 grid minor;
-ylabel('error a','FontSize',72)
+ylabel('error a','FontSize',36)
 subplot(3,2,3)
 plot(t,b*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
 plot(t,thetabarArray(7,:)/dt,':g','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 grid on;
 grid minor;
 ylim([0 0.2])
-ylabel('b','FontSize',72)
+ylabel('b','FontSize',36)
 subplot(3,2,4)
 plot(t,b*ones(1,length(t))-thetabarArray(7,:)/dt,':r','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 grid on;
 grid minor;
-ylabel('error b','FontSize',72)
+ylabel('error b','FontSize',36)
 subplot(3,2,5)
 plot(t,c*ones(1,length(t)),'-k','LineWidth',10);
 hold on;
 plot(t,-thetabarArray(10,:)/dt,':g','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 grid on;
 grid minor;
 ylim([10 20])
-ylabel('c','FontSize',72)
+ylabel('c','FontSize',36)
 xlabel('t (s)')
 subplot(3,2,6)
 plot(t,-c*ones(1,length(t))-thetabarArray(10,:)/dt,':r','LineWidth',10);
-set(gca,'color','white','LineWidth',3,'FontSize',36)
+set(gca,'color','white','LineWidth',3,'FontSize',24)
 grid on;
 grid minor;
-ylabel('error c','FontSize',72)
+ylabel('error c','FontSize',36)
 xlabel('t (s)')
